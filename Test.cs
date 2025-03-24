c96041a5f7920fe836d813e4629303ee16bb03d2
Dionisie4 <dionisiehagiu2@gmail.com>
2025-03-24 17:37:15 +0200

    Update Test.cs

--- Test.cs
+++ Test.cs
@@ -13,5 +13,11 @@ namespace Proiect_PIU
         public string variante_raspuns; //conform index_intrebare iau si varianta de raspuns aferenta enuntului
         public char indice_raspuns_corect; // raspunsul la intrebare va consta dintr un indice (a,b,c) la finaul
                                            // enuntului in fisierul cu intrebari
+
+        // o sa am o functie de genul ManagerIntrebare(Index) care o sa ia ca parametru index ul si apoi 
+        //se va uita pe fisierul intrebari.txt dupa intrebarea cu indexul potrivit
+        //si va printa intrebarea, apoi se va uita si pe raspusuri.txt tot dupa index ul grupurilor de raspuns si 
+        //si va printa raspunsurile
+        //
     }
 }
