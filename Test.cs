fb16a494fd3db710db6d8ed88a0e69880fe91415
Dionisie4 <dionisiehagiu2@gmail.com>
2025-03-24 10:24:28 +0200

    Add project files.

---
+++ Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect_PIU
+{
+    class Test
+    {
+        public int index_intrebare;
+        public string enunt; //iau enuntul intrebarii conform index_intrebare
+        public string variante_raspuns; //conform index_intrebare iau si varianta de raspuns aferenta enuntului
+        public char indice_raspuns_corect; // raspunsul la intrebare va consta dintr un indice (a,b,c) la finaul
+                                           // enuntului in fisierul cu intrebari
+    }
+}
